bfb4ec26cc5c959ed0eadbac0138630f57c7e92d
pews <iampiyushthakur007@gmail.com>
2023-09-09 14:53:49 +0530

    started strivers playlist

---
+++ 2023/CPP/basicio.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// int main()
+// {
+//     cout << "hello world";
+//     cout << "\n";
+//     cout << "hello world";
+//     cout << '\n';
+//     return 0;
+// }
+
+ int main()
+ {
+    int x,y;
+    cin >> x >> y;
+    cout << "value of x is " << x << " and the value of y is " << y;
+    return 0;
+ }
+ 
